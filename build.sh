bf29c8ecfa605bf558f391c8366dcb32767ce84e
Nicklas Boman <smurfd@gmail.com>
2023-02-04 14:09:31 +0100

    fix build

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 mkdir build
-cd build
-gcc  -flat_namespace -I/opt/pkg/include/libusb-1.0/ -L/opt/pkg/lib/ -lusb-1.0 ../lightkey.c -o lightkey
-DYLD_FORCE_FLAT_NAMESPACE=1 ./lightkey
-cd ..
+gcc  -flat_namespace -I/opt/pkg/include/libusb-1.0/ -L/opt/pkg/lib/ -lusb-1.0 lightkey.c -o build/lightkey
+DYLD_FORCE_FLAT_NAMESPACE=1 ./build/lightkey
 rm -rf build
